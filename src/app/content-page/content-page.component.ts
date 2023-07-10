15ec3c047646ad7bce9359c65e64d3286be26255
vraj129 <vrajpatelv@protonmail.com>
2023-07-10 16:49:06 +0530

    [FEATURE] -- Contact Page

--- src/app/content-page/content-page.component.ts
+++ src/app/content-page/content-page.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Listing } from '../types';
+import { fakeListings } from '../fake-data';
 
 @Component({
   selector: 'app-content-page',
   templateUrl: './content-page.component.html',
-  styleUrls: ['./content-page.component.css']
+  styleUrls: ['./content-page.component.css'],
 })
-export class ContentPageComponent {
+export class ContentPageComponent implements OnInit {
+  email: string = '';
+  message: string = '';
+  listing?: Listing;
 
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) {}
+  ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    this.listing = fakeListings.find((listing) => {
+      return listing.id === id;
+    });
+    this.message = `Hi, I'm interested in your ${this.listing?.name.toLowerCase()}!`;
+  }
+
+  sendMessage(): void {
+    alert('Your message has been sent');
+    this.router.navigateByUrl('/listings');
+  }
 }
--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fakeMyListings } from '../fake-data';
+import { Listing } from '../types';
 
 @Component({
   selector: 'app-my-listings-page',
   templateUrl: './my-listings-page.component.html',
-  styleUrls: ['./my-listings-page.component.css']
+  styleUrls: ['./my-listings-page.component.css'],
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting : ${listingId}`);
+  }
 }
